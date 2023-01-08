189771028e5267fe9149ea7563eb5764a5c719fa
Mars Ray Araullo <106045089+marsrayfs@users.noreply.github.com>
2023-01-09 01:38:43 +0800

    Updating version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.2
+* Added registration feature (Firebase)
+
 ## 0.0.1
 
 * TODO: Describe initial release.
--- lib/presentation/register_bloc/register_bloc.dart
+++ lib/presentation/register_bloc/register_bloc.dart
@@ -1,7 +1,7 @@
 library standalone_pkg;
 
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:standalone_pkg/domain/usecase/register_usecase.dart';
 
 import '../../util/result.dart';
