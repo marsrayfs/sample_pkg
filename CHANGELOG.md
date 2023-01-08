5593ac5d1f67eb8807039eefb0b66eed8078ebbd
Mars Ray Araullo <106045089+marsrayfs@users.noreply.github.com>
2023-01-09 01:45:03 +0800

    Updating version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.3
+* Updated repository constructor
+
 ## 0.0.2
 * Added registration feature (Firebase)
 
--- lib/domain/repository/firebase_register_repository_impl.dart
+++ lib/domain/repository/firebase_register_repository_impl.dart
@@ -1,10 +1,11 @@
 library standalone_pkg;
 
+import 'package:standalone_pkg/data/datasource/registration/firebase_registration_datasource.dart';
 import 'package:standalone_pkg/data/repository/register_repository.dart';
 import 'package:standalone_pkg/util/result.dart';
 
 class FirebaseRegisterRepositoryImpl extends RegisterRepository {
-  FirebaseRegisterRepositoryImpl(super.dataSource);
+  FirebaseRegisterRepositoryImpl() : super(FirebaseRegistrationDataSource());
 
   @override
   Future<Result> register(String email, String password) {
